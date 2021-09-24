c3ad91815c11d55d23be912fbbda224c30e42b3a
Diegofigueiredo1 <60989999+Diegofigueiredo1@users.noreply.github.com>
2021-09-24 15:47:10 -0300

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,8 +1,18 @@
 const domain = 'http://localhost:3000';
 
 
-async function read(resource) {
-  const res = await fetch(`${domain}/${resource}`);
+async function read(resource, path, config) {
+  const res = await fetch(`${domain}/${resource}`,{
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      path: path,
+      config: config
+    })
+
+  });
 
   return await res.json();
 }
