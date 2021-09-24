6402008d5f7c166e27159d60afb8c68b7842e2fe
Diegofigueiredo1 <60989999+Diegofigueiredo1@users.noreply.github.com>
2021-09-24 15:50:57 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import express from "express"
 import { getDir } from "../lib/client.js"
 
+
 const router = express.Router();
 
-router.get("/sftp", async (req, res) => {
-    const path = req.query.list;
-    let fileList = await getDir(path);
+router.post("/sftp", async (req, res) => {
+
+    const {
+        config,
+        path
+    } = req.body
+    let fileList = await getDir(path, config);
     res.json(fileList);
-});
+ });
 
-// req.body
-// routes.get("/sftp", async (req, res) => {
-//     const { path } = req.body;
-//     let fileList = await getDir(path)
-//     res.json(fileList);
-// });
-
-export { router };
+ export { router };
