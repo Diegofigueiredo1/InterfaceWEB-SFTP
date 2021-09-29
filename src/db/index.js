4da35a9e18d6220a0d538eb7eb00983190094576
Diegofigueiredo1 <60989999+Diegofigueiredo1@users.noreply.github.com>
2021-09-29 15:50:10 -0300

    Update index.js

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import Database from "sqlite-async";
+
+import Seed from "./seeders.js";
+import Migration from "./migrations.js";
+
+const databaseFile = path.resolve("src", "db", "database.sqlite");
+
+async function connect() {
+  return await Database.open(databaseFile);
+}
+
+async function load() {
+  if (!fs.existsSync(databaseFile)) {
+    Migration.up();
+    Seed.up();
+  }
+}
+
+export default { connect, load };
