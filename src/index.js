6509beaa5374164e24759d8e7b2988f9e284c4b3
jefferson-LFS <jefferson.altere@gmail.com>
2021-09-30 20:18:25 -0300

    Update routes - user registration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+import "express-async-errors";
 import express from "express";
-// import nunjucks from "nunjucks";
 import cors from "cors";
 import morgan from "morgan";
+import Database from "./db/index.js";
 import { router } from "./router/index.js"
+
 
 const app = express();
 
 app.use(express.json());
-// app.use(express.static('public'));
+
+app.use(express.static("public"));
 
 app.use(cors());
 
@@ -15,14 +18,10 @@ app.use(morgan());
 
 app.use(router);
 
-// app.set('view engine', 'njk');
+Database.load();
 
-// nunjucks.configure('src/views', {
-//   express: app,
-//   autoescape: true,
-//   noCache: true,
-// });
 
-// Seed.up()
+
+
 app.listen(3000, () => console.log("Server running"));
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import express from "express"
 import { getDir } from "../lib/client.js"
-
+import User from "../models/User.js";
 
 const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.redirect("signup.html");
+});
 
 router.post("/sftp", async (req, res) => {
 
@@ -12,6 +17,19 @@ router.post("/sftp", async (req, res) => {
     } = req.body
     let fileList = await getDir(path, config);
     res.json(fileList);
- });
+});
 
- export { router };
+
+
+
+router.post("/signup", async (req, res) => {
+    const user = req.body;
+
+    const lastID = await User.create(user);
+
+    const newUser = await User.read(lastID);
+
+    res.status(201).json(newUser);
+});
+
+export { router };
